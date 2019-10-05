7f74a0f6b8f60f5883c2f3d5ae8b53acf1db94f4
Benjamin Gruenbaum <benjamingr@gmail.com>
2019-10-05 18:02:26 +0300

    add cursor heuristic and contains button inside heuristic

--- web-to-data/index.ts
+++ web-to-data/index.ts
@@ -1,9 +1,9 @@
 import { getParsedContent, dump } from './read-data';
-import { shuffle, zip, uniq } from 'lodash';
+import { shuffle, uniq } from 'lodash';
 import * as path from 'path';
 import { promises } from 'fs';
 
-const { appendFile, unlink } = promises;
+const { appendFile, unlink, readFile, writeFile } = promises;
 
 (async () => {
   let i = 0;
@@ -11,7 +11,7 @@ const { appendFile, unlink } = promises;
 
   for await (const page of getParsedContent(path.join(__dirname, './10k_results.json'), x => x.toLowerCase().includes('role="button"'))) {
     const roleButtons = Array.from(page.dom.querySelectorAll('[role=button]'));
-    const others = shuffle(Array.from(page.dom.querySelectorAll('body > * > :not([role=button]')));
+    const others = shuffle(Array.from(page.dom.querySelectorAll('body > * > * > :not([role=button]')));
 
     for (const positive of roleButtons) {
       if (!positive) {
@@ -29,6 +29,14 @@ const { appendFile, unlink } = promises;
       }
     }
   }
+  // now, let's read the whole file, shuffle it and split it into test and train
+
+  const buffer = await readFile('./dump/features.json');
+  const shuffled = shuffle(buffer.toString().split('\n'));
+
+  const trainingSize = Math.floor((shuffled.length) * (8 / 10));
+  await writeFile('./dump/features-training.json', shuffled.slice(0, trainingSize).join('\n'));
+  await writeFile('./dump/features-validation.json', shuffled.slice(trainingSize + 1).join('\n'));
 })();
 
 async function appendFeatures(features: ReturnType<typeof getFeatures>, url: string, label: 0 | 1) {
@@ -52,6 +60,24 @@ function getFeatures(element: Element) {
   if (Array.from(element.textContent!, x => x.charCodeAt(0)).some(x => x > 127)) {
     return;
   }
+
+
+  function isMostTextLinks() {
+    if (element.tagName === 'A') { // the element is a link, obviously all its content is in a link
+      return true;
+    }
+    // we ignore the case the element's parent is a link here.
+    const elementTextLength = (element.textContent || "").length;
+    const elementLinkTextLength = Array.from(element.querySelectorAll('a'), x => x.textContent).join('').length; 
+    return ((elementTextLength - elementLinkTextLength) / elementTextLength) > 0.5;
+  }
+  function areThereButtonsInside() {
+    return element.querySelectorAll('button').length > 0;
+  }
+  function containsHandCursor() {
+    return element.ownerDocument!.defaultView!.getComputedStyle(element).cursor === 'pointer';
+  }
+  
   return {
     tagName: element.tagName.toUpperCase(), // jsdom has a bug where sometimes tagnames are not returned as uppercase per spec
     parentTagName: element.parentElement!.tagName.toUpperCase(),
@@ -63,9 +89,24 @@ function getFeatures(element: Element) {
     // isModal: element.getAttribute('aria-modal') === 'true',
     name: element.getAttribute('name'),
     hasDataset: attributes.some(x => x.startsWith('data-')),
+    isMostTextLinks: isMostTextLinks(),
+    containsHandCursor: containsHandCursor(),
+    areThereButtonsInside: areThereButtonsInside (),
     id: element.id,
     classList: Array.from(element.classList).join(' '),
     descendentsClassList: uniq(Array.from(element.querySelectorAll('*'), x => x.classList).flat().map(x => x.value.split(' ')).flat()).join(' '),
     tagNamesWithin: Array.from(new Set(Array.from(element.querySelectorAll('*'), x => x.tagName.toUpperCase()))).slice(0, 10).join(' '),
   };
+}
+
+function getTree(elementToMark: Element, element: Element) {
+  let text = `(${element.tagName} `;
+
+  if (element === elementToMark) {
+    text += ' MARK ' 
+  }
+  if (element.children.length > 0) {
+    text += `(${Array.from(element.children, getTree.bind(null, elementToMark))})`;
+  }
+  return text + `)`;
 }
